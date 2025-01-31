fa8750db38d9bc68684b49d8b99dc056aa03359a
William Sylvestre <farakwarts@gmail.com>
2025-01-31 15:12:15 -0500

    Add Merge Sort
    Actived when press "m"

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -60,16 +60,77 @@ void insertionSort(std::vector<int>& arr)
 		arr[j + 1] = key;
 	}
 }
+
+// Tri Fusion
+void merge(std::vector<int>& arr, int start, int mid, int end)
+{
+	std::vector<int> tempArr;
+	int i = start;
+	int j = mid + 1;
+
+	while (i <= mid && j <= end)
+	{
+		if (arr[i] <= arr[j])
+		{
+			tempArr.push_back(arr[i++]);
+		}
+		else
+		{
+			tempArr.push_back(arr[j++]);
+		}
+	}
+
+	while (i <= mid)
+	{
+		tempArr.push_back(arr[i++]);
+	}
+
+	while (j <= end)
+	{
+		tempArr.push_back(arr[j++]);
+	}
+
+	for (int k = 0; k < tempArr.size(); k++)
+	{
+		arr[start + k] = tempArr[k];
+	}
+}
+
+void mergeSort(std::vector<int>& arr, int start, int end)
+{
+	if (start < end)
+	{
+		int mid = start + (end - start) / 2;
+		mergeSort(arr, start, mid);
+		mergeSort(arr, mid + 1, end);
+		
+		merge(arr, start, mid, end);
+	}
+}
+
+void mergeSort(std::vector<int>& arr)
+{
+	mergeSort(arr, 0, arr.size() - 1);
+}
+
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+	// Active le Tri à Bulles
 	if (key == 'b')
 	{
 		bubbleSort(circles);
 	}
 
+	// Active le Tri par Insertion
 	if (key == 'i')
 	{
 		insertionSort(circles);
+	}
+
+	// Active le Tri Fusion
+	if (key == 'm')
+	{
+		mergeSort(circles);
 	}
 }
 
